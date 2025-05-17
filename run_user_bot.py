eae7770b31a719e61ee0a1e0136d7f63aea6c7df
TarAn <9986544@gmail.com>
2025-05-17 21:55:15 +0300

    кодировка бота

--- run_user_bot.py
+++ run_user_bot.py
@@ -5,6 +5,10 @@ Run bot for users
 import sys
 import os
 import logging
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 
 # Отключаем подробные логи httpx/urllib3
 logging.getLogger("httpx").setLevel(logging.WARNING)
