30babfb8b1083d681d19fd2b69ede7f0980e4b91
TarAn <9986544@gmail.com>
2025-05-20 12:39:56 +0300

    Пояснения и логировние предсказаний

--- run_predictor.bat
+++ run_predictor.bat
@@ -1,2 +1,4 @@
 python -m src.scripts.predictor --mode train
 python -m src.scripts.predictor --mode predict
+python -m src.scripts.evaluate_predictions --period all
+python -m src.scripts.evaluate_predictions --period week
