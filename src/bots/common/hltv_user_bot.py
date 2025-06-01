8382324d060cab9b6da29fa392b016b7b1badbd7
TarAn <9986544@gmail.com>
2025-06-01 15:04:24 +0300

    фикс текста

--- src/bots/common/hltv_user_bot.py
+++ src/bots/common/hltv_user_bot.py
@@ -1041,7 +1041,7 @@ class HLTVUserBot:
         reply_markup_kb = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
         # Новое: сообщение 'Live матчи' перед списком live-матчей
         if matches:
-            await update.message.reply_text("Live матчи.\nВыберите тип уведления, чтоб получать уведомления о матче.", reply_markup=reply_markup_kb)
+            await update.message.reply_text("Live матчи.\nВыберите тип уведомления, чтоб получать уведомления о матче.", reply_markup=reply_markup_kb)
         else:
             await update.message.reply_text("В текущий момент live матчей нет.", reply_markup=reply_markup_kb)
         # --- Inline-кнопки для live-матчей ---
